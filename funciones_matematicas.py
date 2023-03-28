08805df9d77b54c1a562582cd7de199590e8ccfa
PARADIGMA\QiDimMak <jorgecardona@utp.edu.co>
2023-03-28 07:04:05 -0500

    adicionada funcion resta

--- funciones_matematicas.py
+++ funciones_matematicas.py
@@ -2,11 +2,17 @@ def suma(x, y):
     
     resultado =  x + y
     print(f'el resultado de la SUMA {x} + {y} es {resultado}')
+
+def resta(x, y):
     
+    resultado =  x - y
+    print(f'el resultado de la RESTA  {x} - {y}es {resultado}')
+        
 def ejecutar_funciones():
     
     x, y = 7, 5
     suma(x, y)
+    resta(x, y)
                                   
 if __name__ == "__main__":
     ejecutar_funciones()
