d8f1222709a3d23d39d55583f16c2411d8b630f8
PARADIGMA\QiDimMak <jorgecardona@utp.edu.co>
2023-03-28 07:04:50 -0500

    adicionada funcion multiplicacion

--- funciones_matematicas.py
+++ funciones_matematicas.py
@@ -7,12 +7,18 @@ def resta(x, y):
     
     resultado =  x - y
     print(f'el resultado de la RESTA  {x} - {y}es {resultado}')
-        
+
+def multiplicacion(x, y):
+    
+    resultado =  x * y
+    print(f'el resultado de la MULTIPLICACION  {x} * {y} es {resultado}')
+            
 def ejecutar_funciones():
     
     x, y = 7, 5
     suma(x, y)
     resta(x, y)
+    multiplicacion(x, y)
                                   
 if __name__ == "__main__":
     ejecutar_funciones()
