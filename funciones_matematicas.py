00d5b10bc53e1f20df76422492fce9cdaf96eb40
PARADIGMA\QiDimMak <jorgecardona@utp.edu.co>
2023-03-28 07:02:49 -0500

    adicionada funcion suma

---
+++ funciones_matematicas.py
@@ -0,0 +1,12 @@
+def suma(x, y):
+    
+    resultado =  x + y
+    print(f'el resultado de la SUMA {x} + {y} es {resultado}')
+    
+def ejecutar_funciones():
+    
+    x, y = 7, 5
+    suma(x, y)
+                                  
+if __name__ == "__main__":
+    ejecutar_funciones()
